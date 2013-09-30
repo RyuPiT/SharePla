a6ccddfd8e59ec22da8c936eb1dda562699715f2
Yoshiaki OSHIRO <himajin315@ns.ie.u-ryukyu.ac.jp>
2013-09-30 07:49:03 +0000

    add jquery-ui

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'jquery-ui-rails'
 
 # Twitter Bootstrap 3.0
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'jquery-turbolinks'
 
 
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.ui.all
 //= require_tree .
 
+ $(function() {
+ $( "#draggable" ).draggable();
+});
