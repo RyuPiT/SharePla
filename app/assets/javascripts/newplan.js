53ffca3f673aef883f7122c6d92213a5f2d1e5c1
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-10-08 16:26:37 +0900

    Update newplan.js and newplan_controller add action => when "save" button click, save json

--- app/assets/javascripts/newplan.js
+++ app/assets/javascripts/newplan.js
@@ -20,3 +20,28 @@ function addplan_callback(data) {
   $("#sortable").append(li);
   $("#addplan > input[name=keyword]").val("");
 };
+
+$(function() {
+  $("#saveplan").submit(function() {
+    var all_card = get_all_card();
+    jQuery.post("/newplan/save.json", { "all_card": all_card, authenticity_token: getCSRFtoken() }, saveplan_callback, "text")
+    .fail(
+      function() {
+      alert("save plan post failed");
+      }
+    );
+    return false;
+  });
+});
+
+function saveplan_callback(data) {
+  alert("post ok");
+};
+
+function get_all_card() {
+  var arr = new Array();
+  for(var i = 0 ; i < $("#sortable > li").length ; i++){
+    arr[i] = $("#sortable > li").eq(i).text();
+  }
+  return arr;
+}
--- app/controllers/newplan_controller.rb
+++ app/controllers/newplan_controller.rb
@@ -9,4 +9,27 @@ class NewplanController < ApplicationController
       format.json { render :json => @planlist }
     end
   end
+
+  def save
+    i = 0
+    id = 0
+    day_id = "day" + id.to_s
+
+    day_plan = { day_id => { } }
+
+    params['all_card'].each do |card|
+      card_list = { "title" => card }
+      day_plan[day_id].store(i,card_list)
+      i += 1
+    end
+
+    open("tmp/plans.json", "a") do |io|
+      JSON.dump(day_plan, io)
+    end
+
+    respond_to do |format|
+      format.html { render :nothing => true }
+      format.json { render :nothing => true }
+    end
+  end
 end
