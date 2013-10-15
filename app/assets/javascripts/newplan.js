3ac5538971164b0e89d13b563147993aae9fe23b
himajin315 <himajin315@gmail.com>
2013-10-15 03:35:30 +0000

    Add list-delete-button

--- app/assets/javascripts/newplan.js
+++ app/assets/javascripts/newplan.js
@@ -13,7 +13,8 @@ $(function() {
   function addplanCallback(data) {
     var li = $("<li>");
     li.addClass("ui-state-default");
-    li.text(data["name"]);
+    li.append("<span class=\"title\">" + data["name"] + "<\/span>");
+    li.append("<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>");
     $("#sortable").append(li);
     $("#addplan > input[name=keyword]").val("");
   }
@@ -37,9 +38,9 @@ $(function() {
 
   function getAllCard() {
     var allCard = new Array();
-    var size     = $("#sortable > li").length;
+    var size     = $("#sortable > li ").length;
     for(var i = 0; i < size; i++){
-      allCard[i] = $("#sortable > li").eq(i).text();
+      allCard[i] = $("#sortable > li > .title").eq(i).text();
     }
     return allCard;
   }
