6bd61580d039d9e799342ac16d54b94bf9aa061d
himajin315 <himajin315@gmail.com>
2013-10-25 02:20:24 +0900

    Fix web-font (heroku) heroku upload test-4 (Firefox) Rails 4

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module SharePla
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += ["fontawesome-webfont.ttf",
-                                 "fontawesome-webfont.eot",
-                                 "fontawesome-webfont.svg",
-                                 "fontawesome-webfont.woff"]
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
