3b01612c8f3b92e29c1ed8a92dca234420f3e10c
Yu SUGIMOTO <k138563@ie.u-ryukyu.ac.jp>
2013-10-25 12:21:36 +0000

    test

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,6 +6,7 @@ class DocumentsController < ApplicationController
 
   def create
     p params[:uri]
+    `wkhtmltopdf http://localhost:3000/plan.5262151c70726508ed000000 test.pdf`
     render :nothing => true
 #    render :pdf => "newplan", :layout =>false, :template => params[:uri], :encoding => "UTF-8"
   end
