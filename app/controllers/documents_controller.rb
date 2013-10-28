5f8f60e5eef9ff1ad23ce474c2cf49e4a0823848
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-10-28 17:32:30 +0900

    Add pdf convert function modified:   documents_controller.rb

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
 class DocumentsController < ApplicationController
+require 'open-uri'
+require 'nokogiri'
   def sample
     render :pdf => "newplan", :layout =>false, :template => "/top/static.html", :encoding => "UTF-8"
   end
@@ -11,28 +12,25 @@ class DocumentsController < ApplicationController
   end
 
   def string
-    pdf = WickedPdf.new.pdf_from_string('<meta http-equiv="content-type" content="text/html; charset=utf-8" /><h1>阿呆</h1><h1>hello</h1><h1>hello</h1>')
-    save_path = Rails.root.join('tmp/pdf', 'ticket.pdf')
+    url  = params[:uri]
+    meta = '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'
+    html = meta + open(url).read
+    pdf  = WickedPdf.new.pdf_from_string(html)
+
+    tmp_path  = 'tmp/pdf'
+    pdf_name  = url.split("/")[-1] + ".pdf"
+    save_path = Rails.root.join(tmp_path, pdf_name)
     File.open(save_path, 'wb') do |file|
       file << pdf
     end
+
+    doc            = Nokogiri::HTML(open(url), nil, 'utf-8')
+    download_title = doc.css('h1')[0].children[0].to_s + '.pdf'
     
-    send_data(File.read('tmp/pdf/ticket.pdf'), :filename => 'hoge.pdf',:disposition=> 'inline')
-    
-    # download pdf file
+   send_data(File.read(save_path), :filename => download_title, :disposition=> 'inline')
+# download pdf file
 #    send_file('tmp/pdf/ticket.pdf') 
 #    render :nothing => true
   end
 end
-
-
-
-
-
-
-
-
-
-
-
 
