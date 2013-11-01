8036a576c0a622148327da6bb2982c14b3a65859
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-11-01 22:45:07 +0000

    Maybe fix build error comment out 'require nokogiri' reason is mulit require? irb> require 'nokogiri' true irb> require 'nokogiri' false

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
 require 'open-uri'
-require 'nokogiri'
+#require 'nokogiri'
   def sample
     render :pdf => "newplan", :layout =>false, :template => "/top/static.html", :encoding => "UTF-8"
   end
@@ -24,8 +24,10 @@ require 'nokogiri'
       file << pdf
     end
 
-    doc            = Nokogiri::HTML(open(url), nil, 'utf-8')
-    download_title = doc.css('h1')[0].children[0].to_s + '.pdf'
+    #doc            = Nokogiri::HTML(open(url), nil, 'utf-8')
+    #download_title = doc.css('h1')[0].children[0].to_s + '.pdf'
+    download_title = "SharePla.pdf"
+
     
    send_data(File.read(save_path), :filename => download_title, :disposition=> 'inline')
 # download pdf file
