ec5e5202af0ed6a7fea1e183491a939d743a5e26
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-11-02 15:34:29 +0900

    Modified:   app/controllers/plan_controller.rb Copy from newplan controller

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -1,4 +1,49 @@
 class PlanController < ApplicationController
   def index
   end
+
+  def add
+    word = { name: params['name'] }
+
+    respond_to do |format|
+      format.html { render nothing: true }
+      format.json { render json: word }
+    end
+  end
+
+  def search_hotel
+    @json_data = RakutenService.hotel_search(params['name'])
+
+    respond_to do |format|
+      format.html { render nothing: true }
+      format.json { render json: @json_data }
+    end
+  end
+
+  def save
+    arr = []
+
+    @plan = Plan.new(plan_params)
+
+    params['plan']['days'].each do |key,value|
+      @plan.days.push(Day.new(value))
+    end
+
+    respond_to do |format|
+      if @plan.save
+        flash[:notice] = "保存しました"
+        format.html { render nothing: true }
+        format.json { render nothing: true }
+      end
+    end
+  end
+
+  private
+
+  def plan_params
+    {
+      'title'       => params['plan']['title'],
+      'description' => params['plan']['desc']
+    }
+  end
 end
