98010410607ac3d2f630b52118cf4d15b4e48f1f
himajin315 <himajin315@gmail.com>
2013-11-02 20:55:51 +0900

    Remove yoshiaki (alert)

--- app/assets/javascripts/plan.js
+++ app/assets/javascripts/plan.js
@@ -128,7 +128,6 @@ $(document).ready(function(){
 
 
 $(function(){
-    alert("yoshiaki");
     var prefectures = ["北海道",
                        "青森県","岩手県","宮城県","秋田県","山形県","福島県",
                        "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
