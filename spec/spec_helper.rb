04d451ea2ab746303473e2e82fa9e98ea67d74c8
Seiei Higa <hanachin@gmail.com>
2013-11-04 02:54:55 +0900

    ユーザーが新しくプランを作成し、保存すると、トップページにそのプランが表示されるようなストーリーを書いてテストを追加した。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'rails/mongoid'
+require 'capybara/rails'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
