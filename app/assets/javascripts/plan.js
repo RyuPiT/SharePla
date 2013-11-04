04134f15e082dda43d04144d156bbc39d7a3ff02
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-11-04 16:10:41 +0900

    インデント修正 Update:   app/assets/javascripts/plan.js

--- app/assets/javascripts/plan.js
+++ app/assets/javascripts/plan.js
@@ -115,7 +115,7 @@ $(function() {
   $( "ol.dropfalse" ).sortable({
     connectWith: "ol",
     dropOnEmpty: false
-    });
+  });
 
   $( "#main-card-sortable, #hotel-card-sortable" ).disableSelection();
 });
@@ -124,28 +124,28 @@ $(function() {
 
 //textarea autosize
 $(document).ready(function(){
-    $('textarea').autosize();
+  $('textarea').autosize();
 });
 
 
 $(function(){
-    var prefectures = ["北海道",
-                       "青森県","岩手県","宮城県","秋田県","山形県","福島県",
-                       "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
-                       "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県",
-                       "三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
-                       "鳥取県","島根県","岡山県","広島県","山口県",
-                       "徳島県","香川県","愛媛県","高知県",
-                       "福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県",
-                       "沖縄県"];
-   $.each(prefectures, function(){
-       var label = $("<label>");
-       var input = "<input type=\"checkbox\">" + this;
+  var prefectures = ["北海道",
+                     "青森県","岩手県","宮城県","秋田県","山形県","福島県",
+                     "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
+                     "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県",
+                     "三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
+                     "鳥取県","島根県","岡山県","広島県","山口県",
+                     "徳島県","香川県","愛媛県","高知県",
+                     "福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県",
+                     "沖縄県"];
+$.each(prefectures, function(){
+  var label = $("<label>");
+  var input = "<input type=\"checkbox\">" + this;
 
 
-       label.addClass("btn btn-primary");
+  label.addClass("btn btn-primary");
 
-       label.append(input);
-       $("#prefectures").append(label);
-   });
+  label.append(input);
+  $("#prefectures").append(label);
+});
 });
