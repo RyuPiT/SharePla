5332cbcf87b290d71d1bf6c8c342e4b27893bfa7
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-11-05 17:18:59 +0900

    Change create code side, Cosmetic Change html <label> tag, app/assets/javascripts/plan.js -> app/controllers/plan_controller.rb Change tag position each area

--- app/assets/javascripts/plan.js
+++ app/assets/javascripts/plan.js
@@ -120,32 +120,7 @@ $(function() {
   $( "#main-card-sortable, #hotel-card-sortable" ).disableSelection();
 });
 
-
-
 //textarea autosize
 $(document).ready(function(){
   $('textarea').autosize();
-});
-
-
-$(function(){
-  var prefectures = ["北海道",
-                     "青森県","岩手県","宮城県","秋田県","山形県","福島県",
-                     "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
-                     "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県",
-                     "三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
-                     "鳥取県","島根県","岡山県","広島県","山口県",
-                     "徳島県","香川県","愛媛県","高知県",
-                     "福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県",
-                     "沖縄県"];
-$.each(prefectures, function(){
-  var label = $("<label>");
-  var input = "<input type=\"checkbox\">" + this;
-
-
-  label.addClass("btn btn-primary");
-
-  label.append(input);
-  $("#prefectures").append(label);
-});
 });
