ec99d70b28e04638256fa6d16d73471355a52aa8
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-11-15 20:40:29 +0900

    Fix indent
    Modified:   google_maps.js

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -6,56 +6,56 @@ var lat;
 var lng;
 
 function initialize(){
-　if (typeof lat == 'undefined'){
+  　if (typeof lat == 'undefined'){
     //init
     lat = 35.681382;
     lng = 139.766084;
   }
   myLatLng = new google.maps.LatLng(lat, lng);
   var mapOptions = {
-	  center: myLatLng,
-	  zoom:11,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: myLatLng,
+    zoom:11,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
   // ユーザのマーカーアイコンを変更
   var markerImage = new google.maps.MarkerImage(
-	  // 画像の場所
-    "http://blog-imgs-44.fc2.com/p/c/r/pcrice/mark2.png",
-	  // マーカーのサイズ
-	  new google.maps.Size(20, 24),
-	  // 画像の基準位置
-	  new google.maps.Point(0, 0),
-	  // Anchorポイント
-	  new google.maps.Point(10, 24)
-  );
-    
+      // 画像の場所
+      "http://blog-imgs-44.fc2.com/p/c/r/pcrice/mark2.png",
+      // マーカーのサイズ
+      new google.maps.Size(20, 24),
+      // 画像の基準位置
+      new google.maps.Point(0, 0),
+      // Anchorポイント
+      new google.maps.Point(10, 24)
+      );
+
   // 現在地のマーカー表示
   var marker = new google.maps.Marker({
-	  map:map,
-	  draggable:false,
-	  animation: google.maps.Animation.DROP,
-	  position: myLatLng,
-	  title: "現在地",
-	  icon: markerImage
+    map:map,
+      draggable:false,
+      animation: google.maps.Animation.DROP,
+      position: myLatLng,
+      title: "現在地",
+      icon: markerImage
   });
-    
+
 }
 
 
 // マップオブジェクトを作成し、マーカーを表示
 function textSearch(searchWard){
-　latlng = map.getCenter();
-　lat = latlng.lat();
-　lng = latlng.lng();
+  　latlng = map.getCenter();
+  　lat = latlng.lat();
+  　lng = latlng.lng();
   initialize();
-  
+
   // テキスト検索
   var request = {
-	  location: myLatLng,
-	  radius: '10000',
-	  query: searchWard
+    location: myLatLng,
+    radius: '10000',
+    query: searchWard
   };
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
