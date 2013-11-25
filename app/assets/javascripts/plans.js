68bf4643043d145474e6a5a1f0d01d604eb76f72
himajin315 <himajin315@gmail.com>
2013-11-25 13:55:44 +0900

    Rename distination -> message

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -239,7 +239,7 @@ $(function() {
     dropOnEmpty: false
   });
 
-  $('#main-card-sortable, #hotel-card-sortable, #distination-card-sortable').disableSelection();
+  $('#main-card-sortable, #hotel-card-sortable, #message-card-sortable').disableSelection();
   $('#main-card-sortable').droppable({
     activeClass: 'ui-state-hover',
     hoverClass: 'ui-state-active'
