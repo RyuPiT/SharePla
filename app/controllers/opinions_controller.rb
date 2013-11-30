3665234fe0d4f07d615178d78835f0702c1088ce
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-11-30 15:00:35 +0900

    Opinionからトップページへ戻った時にflashが引き継がれている問題に対応

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -10,7 +10,7 @@ class OpinionsController < ApplicationController
 
     respond_to do |format|
       if @opinion.save
-        flash[:notice] = '投稿しました'
+        flash.now[:notice] = '投稿しました'
         index # @new_opinionの初期化と@opinionsの再設定
         format.html { render action: 'index' }
       end
