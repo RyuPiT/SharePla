60a762372157fc5c617e6dc77264250b8f994e27
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-12-02 20:48:32 +0900

    Userをcollectionsに追加できるようにする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   def callback
+    auth = request.env['omniauth.auth']
+    p auth
+    user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User
   field :uid,         type: String
   field :screen_name, type: String
   field :name,        type: String
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider    = auth[:provider]
+      user.uid         = auth[:uid]
+      user.screen_name = auth[:extra][:raw_info][:screen_name]
+      user.name        = auth[:info][:name]
+    end
+  end
 end
