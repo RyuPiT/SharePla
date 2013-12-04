40ec861021536ac916870b45be807b5970390bd4
Yu SUGIMOTO <k138563@ie.u-ryukyu.ac.jp>
2013-12-04 16:17:28 +0000

    Modified:   app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # using nokogiri, Travis is not unstable.
   require 'nokogiri'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
