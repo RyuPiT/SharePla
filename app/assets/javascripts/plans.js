067f620f7525dc1f8607b607ab7364ddbe1f400f
Yu SUGIMOTO <k138563@ie.u-ryukyu.ac.jp>
2013-12-07 02:25:21 +0000

    Test callback method

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -7,9 +7,19 @@ $(function() {
   $('#create-message-card').submit(function() {
     var postData = { name: $('#create-message-card input[name=keyword]').val() };
     addCardToPlan(postData);
-    focusAddPlanCard();
+    yahoo();
     return false;
   });
+  
+  function yahoo(){test(function(){
+    alert();
+    $('.plan-detail').scrollTop($('.plan-detail')[0].scrollHeight);
+  })
+  function test(callback){
+    alert();
+    callback();
+    }
+  }
 
   function addCardToPlan(data) {
     var li = $("<li>").hide().animate({ pacity:1 }, function() {
@@ -27,17 +37,13 @@ $(function() {
     li.append(addContent);
     $('#new-my-plan-cards').append(li);
     $('#create-message-card input[name=keyword]').val('');
+    return true;
   }
 
-  function focusAddPlanCard(){
-    console.log($('#new-my-plan-cards').height()); // 207
-    console.log($('#new-my-plan-cards')[0].scrollHeight); // 797
-    //buttom ($('.plan-detail').scrollTop()) = $('#new-my-plan-cards')[0].scrollHeight - $('#new-my-plan-cards').height()
-    $('.plan-detail').scrollTop( $('#new-my-plan-cards')[0].scrollHeight + 80);
-  }
   $('.plan-detail').scroll(function(){
-      console.log($('.plan-detail').scrollTop());
+    //$('.plan-detail').scrollTop($('.plan-detail')[0].scrollHeight);
   });
+
   // data = plans_controller's @json_data = services/api_service.rb's formated_data
   function apiCallback(data) {
     var cardType   = data['meta']['type'];
