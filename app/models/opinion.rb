7db5763408048668e6654d5a4378bf93acadc05b
Yu SUGIMOTO <k138563@ie.u-ryukyu.ac.jp>
2013-12-07 06:13:59 +0000

    Add field :writer on opinion model

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -5,6 +5,9 @@ class Opinion
   field :comment, type: String
   field :title,   type: String
   field :likes,   type: Integer, default: 0
+  field :writer,  type: String
+
+  embedded_in :plan
 
   validates :comment, presence: true
 end
