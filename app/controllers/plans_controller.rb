3df6b2eead0797d2d23e9c5568517d41b8c1c676
himajin315 <himajin315@gmail.com>
2013-12-11 15:07:07 +0900

    Fix bug users_info Add users_info to controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -16,6 +16,7 @@ class PlansController < ApplicationController
   end
 
   def show
+    @users    = users_info
     respond_to do |format|
       format.pdf do
         send_data render_to_pdf, filename: "#{@plan.id}.pdf", disposition: 'inline'
